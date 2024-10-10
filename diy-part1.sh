0b780824876dac4945ad676a8eb9727cbc0fdd26
Mr.Blcakcat <q453974158@gmail.com>
2024-10-10 11:43:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+cd package
+
+#国内常用OpenWrt软件包源码合集
+git clone https://github.com/liuran001/openwrt-packages
+
+#luci-app-pushbot 全能推送
+git clone https://github.com/zzsj0928/luci-app-pushbot
