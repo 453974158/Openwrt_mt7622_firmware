1015313531157f06c175e0d9d6e8b6be7dfb2fb0
Mr.Blcakcat <q453974158@gmail.com>
2024-10-12 02:00:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,8 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -23,8 +23,6 @@
 
 #国内常用OpenWrt软件包源码合集
 #git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/kenzok8/small
 
 #luci-app-pushbot 全能推送
 #git clone https://github.com/zzsj0928/luci-app-pushbot
